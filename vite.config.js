00663d00a4aa39c9aeef8577ab004f562b51f8bc
ひらとしんじ <snj.hirato@gmail.com>
2024-05-05 21:46:03 +0900

    依存関係解決

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
+  optimizeDeps: {
+    include: ['@react-spring/shared']
+  }
 })
+
